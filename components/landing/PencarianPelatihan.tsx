efe3661be8905e134a6fcabc11b929d11746072a
gustonecrush <codeboxezz@gmail.com>
2025-02-13 22:21:00 +0700

    push

--- components/landing/PencarianPelatihan.tsx
+++ components/landing/PencarianPelatihan.tsx
@@ -442,7 +442,7 @@ function PencarianPelatihan() {
                       </div>
                     </div>
                   ) : (
-                    <div className="w-full grid grid-cols-1 gap-3  justify-center md:grid-cols-3 ">
+                    <div className="w-full grid grid-cols-1 gap-3  justify-center  ">
                       {data.map((pelatihan, index) => (
                         <CardPelatihan key={index} pelatihan={pelatihan} />
                       ))}
@@ -458,240 +458,175 @@ function PencarianPelatihan() {
   );
 }
 
-// const CardPelatihan = ({ pelatihan }: { pelatihan: PelatihanMasyarakat }) => {
-//   return (
-//     <div className="bg-white shadow-custom text-black p-4 rounded-xl grid grid-cols-1 md:grid-cols-5 mb-4 items-center">
-//       {/* Train Info */}
-//       <div className="max-w-xs leading-[115%]">
-//         <h3 className="text-xl font-bold">{pelatihan.NamaPelatihan}</h3>
-//         <p className="text-sm">{pelatihan.BidangPelatihan} (S)</p>
-//       </div>
-
-//       {/* Departure Info */}
-//       <div className="text-left md:text-center">
-//         <p className="font-bold">{pelatihan.PenyelenggaraPelatihan}</p>
-//         <p className="text-sm leading-[100%]">{pelatihan.LokasiPelatihan}</p>
-//       </div>
-
-//       {/* Arrow and Duration */}
-//       <div className="hidden md:flex flex-col items-start md:items-center">
-//         <div className="bg-blue-500 p-2 rounded-full">
-//           <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             fill="none"
-//             viewBox="0 0 24 24"
-//             strokeWidth={2}
-//             stroke="#000"
-//             className="w-6 h-6 text-white stroke-white"
-//           >
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               d="M13 5l7 7-7 7M5 12h14"
-//               className="text-white"
-//             />
-//           </svg>
-//         </div>
-//       </div>
-
-//       {/* Arrival Info */}
-//       <div className="text-left md:text-center">
-//         <p className="font-bold">{pelatihan.PelaksanaanPelatihan}</p>
-//         {pelatihan!.TanggalMulaiPelatihan != "" ? (
-//           <span className="text-sm leading-[100%] block">
-//             {generateTanggalPelatihan(pelatihan.TanggalMulaiPelatihan)} -{" "}
-//             {generateTanggalPelatihan(pelatihan.TanggalBerakhirPelatihan)}
-//           </span>
-//         ) : (
-//           <span className="text-sm leading-[100%] block">
-//             Waktu Pendaftaran <br />
-//             {generateTanggalPelatihan(pelatihan.TanggalMulaiPendaftaran)} -{" "}
-//             {generateTanggalPelatihan(pelatihan!.TanggalBerakhirPendaftaran)}
-//           </span>
-//         )}
-//       </div>
-
-//       {/* Price and Button */}
-//       <div className="text-center flex md:items-center md:justify-center flex-col">
-//         {pelatihan?.StatusApproval != "Selesai" && (
-//           <p className="text-blue-500 text-left text-xl font-bold">
-//             {formatToRupiah(pelatihan.HargaPelatihan)}
-//           </p>
-//         )}
-
-//         <Link
-//           onClick={(e) => Cookies.set("JenisProgram", pelatihan?.JenisProgram)}
-//           href={`/layanan/pelatihan/${createSlug(pelatihan.NamaPelatihan)}/${
-//             pelatihan?.KodePelatihan
-//           }/${pelatihan?.IdPelatihan}`}
-//           className={`${
-//             pelatihan?.StatusApproval == "Selesai"
-//               ? "bg-gray-500"
-//               : "bg-blue-500"
-//           } text-white px-4 py-2 text-base rounded-md mb-1 mt-2 w-full md:w-fit block`}
-//         >
-//           {pelatihan.StatusApproval == "Selesai"
-//             ? "Sudah Selesai"
-//             : "Lihat Detail"}
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
 const CardPelatihan = ({ pelatihan }: { pelatihan: PelatihanMasyarakat }) => {
   return (
-    <div className="shadow-custom flex flex-col relative w-[380px] h-fit rounded-3xl bg-white p-6">
-      <div className="w-full h-[200px] relative">
-        <Image
-          className="w-full !h-[200px] rounded-2xl object-cover shadow-custom mb-2"
-          alt=""
-          src={replaceUrl(pelatihan?.FotoPelatihan!)}
-          width={0}
-          height={0}
-        />
-        {pelatihan.PenyelenggaraPelatihan.includes("Politeknik") && (
-          <span className="w-fit block text-center font-semibold px-4 py-2 bg-blue-600 rounded-3xl text-white absolute text-xs top-3 z-50 right-3">
-            Khusus Taruna KP
+    <div className="bg-white shadow-custom text-black p-4 rounded-xl grid grid-cols-1 md:grid-cols-5 mb-4 items-center">
+      {/* Train Info */}
+      <div className="max-w-xs leading-[115%]">
+        <h3 className="text-xl font-bold">{pelatihan.NamaPelatihan}</h3>
+        <p className="text-sm">{pelatihan.BidangPelatihan} (S)</p>
+      </div>
+
+      {/* Departure Info */}
+      <div className="text-left md:text-center">
+        <p className="font-bold">{pelatihan.PenyelenggaraPelatihan}</p>
+        <p className="text-sm leading-[100%]">{pelatihan.LokasiPelatihan}</p>
+      </div>
+
+      {/* Arrow and Duration */}
+      <div className="hidden md:flex flex-col items-start md:items-center">
+        <div className="bg-blue-500 p-2 rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={2}
+            stroke="#000"
+            className="w-6 h-6 text-white stroke-white"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13 5l7 7-7 7M5 12h14"
+              className="text-white"
+            />
+          </svg>
+        </div>
+      </div>
+
+      {/* Arrival Info */}
+      <div className="text-left md:text-center">
+        <p className="font-bold">{pelatihan.PelaksanaanPelatihan}</p>
+        {pelatihan!.TanggalMulaiPelatihan != "" ? (
+          <span className="text-sm leading-[100%] block">
+            {generateTanggalPelatihan(pelatihan.TanggalMulaiPelatihan)} -{" "}
+            {generateTanggalPelatihan(pelatihan.TanggalBerakhirPelatihan)}
+          </span>
+        ) : (
+          <span className="text-sm leading-[100%] block">
+            Waktu Pendaftaran <br />
+            {generateTanggalPelatihan(pelatihan.TanggalMulaiPendaftaran)} -{" "}
+            {generateTanggalPelatihan(pelatihan!.TanggalBerakhirPendaftaran)}
           </span>
         )}
       </div>
 
-      {/* Header */}
-      <div className="flex justify-between items-center gap-3 mt-3">
-        <h2 className="text-2xl font-calsans text-blue-500 leading-none">
-          {truncateText(pelatihan?.NamaPelatihan, 50, "...")}
-        </h2>
-        {/* <div className="text-sm font-medium w-fit px-4 py-2 bg-[#625BF9] rounded-3xl text-white leading-none">
-          {generateTanggalPelatihan(pelatihan.TanggalMulaiPelatihan)}
-        </div> */}
+      {/* Price and Button */}
+      <div className="text-center flex md:items-center md:justify-center flex-col">
+        {pelatihan?.StatusApproval != "Selesai" && (
+          <p className="text-blue-500 text-left text-xl font-bold">
+            {formatToRupiah(pelatihan.HargaPelatihan)}
+          </p>
+        )}
+
+        <Link
+          onClick={(e) => Cookies.set("JenisProgram", pelatihan?.JenisProgram)}
+          href={`/layanan/pelatihan/${createSlug(pelatihan.NamaPelatihan)}/${
+            pelatihan?.KodePelatihan
+          }/${pelatihan?.IdPelatihan}`}
+          className={`${
+            pelatihan?.StatusApproval == "Selesai"
+              ? "bg-gray-500"
+              : "bg-blue-500"
+          } text-white px-4 py-2 text-base rounded-md mb-1 mt-2 w-full md:w-fit block`}
+        >
+          {pelatihan.StatusApproval == "Selesai"
+            ? "Sudah Selesai"
+            : "Lihat Detail"}
+        </Link>
       </div>
-
-      {/* Location */}
-      <div className="flex items-center gap-2 text-blue-500 mb-4">
-        <TbMapPin size={18} />
-        <p className="text-sm font-medium">{pelatihan.LokasiPelatihan}</p>
-      </div>
-
-      {/* Description */}
-      <p
-        dangerouslySetInnerHTML={{
-          __html:
-            pelatihan && truncateText(pelatihan?.DetailPelatihan, 150, "..."),
-        }}
-        className="text-gray-600 text-sm leading-relaxed mb-4"
-      />
-
-      {/* Contact Info */}
-      {pelatihan!.PenyelenggaraPelatihan.includes("Politeknik") ? (
-        <></>
-      ) : (
-        <div className="flex justify-between text-sm text-blue-500 mb-4">
-          <div>
-            <p className="font-semibold">
-              Layanan {pelatihan.PenyelenggaraPelatihan}
-            </p>
-            <p>62887972983</p>
-          </div>
-          <div>
-            <p className="font-semibold">
-              PTSP {pelatihan.PenyelenggaraPelatihan}
-            </p>
-            <p>62889812833</p>
-          </div>
-        </div>
-      )}
-
-      {/* Pricing */}
-      <div className=" mb-4">
-        <p className="text-blue-500 text-3xl font-calsans">
-          {pelatihan.HargaPelatihan === 0
-            ? "Gratis"
-            : `${formatToRupiah(pelatihan.HargaPelatihan)}`}
-        </p>
-        <p className="text-sm font-normal text-blue-500">
-          *{" "}
-          {pelatihan!.PenyelenggaraPelatihan.includes("Politeknik")
-            ? "Diperuntukkan untuk taruna Poltek KP dan SUPM"
-            : "Tidak termasuk akomodasi & konsumsi"}{" "}
-          <br />* Kuota Kelas {pelatihan!.KoutaPelatihan} Peserta
-        </p>
-      </div>
-
-      {/* Button */}
-      <Link
-        href={`/layanan/pelatihan/${createSlug(pelatihan.NamaPelatihan)}/${
-          pelatihan?.KodePelatihan
-        }/${encryptValue(pelatihan?.IdPelatihan)}`}
-        className="w-full block text-center font-semibold px-6 py-3 bg-blue-600 rounded-3xl text-white"
-      >
-        Lihat Detail
-      </Link>
     </div>
   );
 };
 
 // const CardPelatihan = ({ pelatihan }: { pelatihan: PelatihanMasyarakat }) => {
 //   return (
-//     <div className="coverflow flex flex-col shadow-custom relative w-fit h-fit rounded-3xl">
-//       <div className="w-fit absolute top-4 right-4 flex gap-1 z-[60]">
-//         <div className="text-xs font-medium px-4 py-2 bg-blue-500 rounded-3xl text-white">
-//           {pelatihan.HargaPelatihan == 0
-//             ? "Gratis"
-//             : formatToRupiah(pelatihan.HargaPelatihan)}
-//         </div>
-//         {/* <div className="text-xs font-medium px-4 py-2 bg-blue-500 rounded-3xl text-white">
-//           {pelatihan.BidangPelatihan}
-//         </div> */}
-//       </div>
-//       <div className="w-full relative h-[240px]">
-//         <div className="flex w-full absolute h-[240px] bg-gradient-to-r opacity-40 from-blue-500 to-teal-400 bg-opacity-40 rounded-tl-3xl rounded-tr-3xl"></div>
+//     <div className="shadow-custom flex flex-col relative w-[380px] h-fit rounded-3xl bg-white p-6">
+//       <div className="w-full h-[200px] relative">
 //         <Image
-//           className="w-full rounded-tl-3xl rounded-tr-3xl  object-cover h-[240px]"
+//           className="w-full !h-[200px] rounded-2xl object-cover shadow-custom mb-2"
 //           alt=""
-//           src={`${replaceUrl(pelatihan.FotoPelatihan)}`}
+//           src={replaceUrl(pelatihan?.FotoPelatihan!)}
 //           width={0}
 //           height={0}
 //         />
+//         {pelatihan.PenyelenggaraPelatihan.includes("Politeknik") && (
+//           <span className="w-fit block text-center font-semibold px-4 py-2 bg-blue-600 rounded-3xl text-white absolute text-xs top-3 z-50 right-3">
+//             Khusus Taruna KP
+//           </span>
+//         )}
 //       </div>
 
-//       <div className="px-6 py-3">
-//         <div className="w-full pb-4">
-//           <h2 className="font-calsans text-xl duration-1000 text-black mt-2 leading-[110%]">
-//             {pelatihan.NamaPelatihan}
-//           </h2>
-//           {/* <div className="flex flex-col">
-//             <span className="flex items-center gap-1 leading-[105%] text-xs text-gray-600">
-//               <span>
-//                 Waktu Pendaftaran :{" "}
-//                 {generateTanggalPelatihan(pelatihan!.TanggalMulaiPendaftaran)}{" "}
-//                 <span className="lowercase">s.d</span>{" "}
-//                 {generateTanggalPelatihan(
-//                   pelatihan!.TanggalBerakhirPendaftaran
-//                 )}
-//               </span>
-//             </span>
-//           </div> */}
-//           <p
-//             dangerouslySetInnerHTML={{
-//               __html:
-//                 pelatihan &&
-//                 truncateText(pelatihan?.DetailPelatihan, 250, "..."),
-//             }}
-//             className="text-sm font-normal group-hover:text-xs text-gray-600 group-hover:duration-1000"
-//           />
+//       {/* Header */}
+//       <div className="flex justify-between items-center gap-3 mt-3">
+//         <h2 className="text-2xl font-calsans text-blue-500 leading-none">
+//           {truncateText(pelatihan?.NamaPelatihan, 50, "...")}
+//         </h2>
+//         {/* <div className="text-sm font-medium w-fit px-4 py-2 bg-[#625BF9] rounded-3xl text-white leading-none">
+//           {generateTanggalPelatihan(pelatihan.TanggalMulaiPelatihan)}
+//         </div> */}
+//       </div>
 
-//           <Link
-//             target="_blank"
-//             href={`/layanan/pelatihan/${createSlug(pelatihan.NamaPelatihan)}/${
-//               pelatihan?.KodePelatihan
-//             }/${pelatihan?.IdPelatihan}`}
-//             className="w-full mt-4 block text-sm text-center font-medium px-6 py-2 bg-blue-500 rounded-3xl text-white"
-//           >
-//             Lihat Detail
-//           </Link>
+//       {/* Location */}
+//       <div className="flex items-center gap-2 text-blue-500 mb-4">
+//         <TbMapPin size={18} />
+//         <p className="text-sm font-medium">{pelatihan.LokasiPelatihan}</p>
+//       </div>
+
+//       {/* Description */}
+//       <p
+//         dangerouslySetInnerHTML={{
+//           __html:
+//             pelatihan && truncateText(pelatihan?.DetailPelatihan, 150, "..."),
+//         }}
+//         className="text-gray-600 text-sm leading-relaxed mb-4"
+//       />
+
+//       {/* Contact Info */}
+//       {pelatihan!.PenyelenggaraPelatihan.includes("Politeknik") ? (
+//         <></>
+//       ) : (
+//         <div className="flex justify-between text-sm text-blue-500 mb-4">
+//           <div>
+//             <p className="font-semibold">
+//               Layanan {pelatihan.PenyelenggaraPelatihan}
+//             </p>
+//             <p>62887972983</p>
+//           </div>
+//           <div>
+//             <p className="font-semibold">
+//               PTSP {pelatihan.PenyelenggaraPelatihan}
+//             </p>
+//             <p>62889812833</p>
+//           </div>
 //         </div>
+//       )}
+
+//       {/* Pricing */}
+//       <div className=" mb-4">
+//         <p className="text-blue-500 text-3xl font-calsans">
+//           {pelatihan.HargaPelatihan === 0
+//             ? "Gratis"
+//             : `${formatToRupiah(pelatihan.HargaPelatihan)}`}
+//         </p>
+//         <p className="text-sm font-normal text-blue-500">
+//           *{" "}
+//           {pelatihan!.PenyelenggaraPelatihan.includes("Politeknik")
+//             ? "Diperuntukkan untuk taruna Poltek KP dan SUPM"
+//             : "Tidak termasuk akomodasi & konsumsi"}{" "}
+//           <br />* Kuota Kelas {pelatihan!.KoutaPelatihan} Peserta
+//         </p>
 //       </div>
+
+//       {/* Button */}
+//       <Link
+//         href={`/layanan/pelatihan/${createSlug(pelatihan.NamaPelatihan)}/${
+//           pelatihan?.KodePelatihan
+//         }/${encryptValue(pelatihan?.IdPelatihan)}`}
+//         className="w-full block text-center font-semibold px-6 py-3 bg-blue-600 rounded-3xl text-white"
+//       >
+//         Lihat Detail
+//       </Link>
 //     </div>
 //   );
 // };
