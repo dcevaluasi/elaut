9d310f4940e669f2156c63057abd1877c98bdccb
gustonecrush <codeboxezz@gmail.com>
2025-02-24 08:04:01 +0700

    added new url

--- constants/urls.ts
+++ constants/urls.ts
@@ -5,5 +5,6 @@ export const blankoAkapiBaseUrl = process.env.NEXT_PUBLIC_BLANKO_AKAPI_URL
 export const kusukaBaseUrl = process.env.NEXT_PUBLIC_KUSUKA_URL
 export const manningAgentDevUrl =
   process.env.NEXT_PUBLIC_BASE_URL_MANNING_AGENT_DEV
+export const akapiBaseUrl = process.env.NEXT_PUBLIC_AKAPI_URL
 
 export const wilayahIDUrl = process.env.NEXT_PUBLIC_WILAYAH_ID_URL
--- hooks/blanko/useFetchSertifikatByLemdiklat.ts
+++ hooks/blanko/useFetchSertifikatByLemdiklat.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from 'axios'
 import Cookies from 'js-cookie'
 import { useState, useEffect } from 'react'
-import { blankoAkapiBaseUrl } from '@/constants/urls'
+import { akapiBaseUrl, blankoAkapiBaseUrl } from '@/constants/urls'
 import { SummarySertifikatByLemdiklat } from '@/types/akapi'
 
 const useFetchSertifikatyByLemdiklat = () => {
@@ -12,7 +12,7 @@ const useFetchSertifikatyByLemdiklat = () => {
     setIsFetching(true)
     try {
       const response: AxiosResponse = await axios.get(
-        `${blankoAkapiBaseUrl}/getBalaiSertifikat`,
+        `${akapiBaseUrl}/get-balai-sertifikat?waktu_awal=2024-06-01&waktu_berakhir=2025-12-31`,
         {
           headers: {
             Authorization: `Bearer ${Cookies.get('XSRF091')}`,
