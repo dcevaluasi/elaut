6d0450b89507431e9d4821daa14276bd906c4098
gustonecrush <codeboxezz@gmail.com>
2025-03-02 23:17:08 +0700

    added firebase config

--- components/dashboard/Pelatihan/TableDataPesertaPelatihan.tsx
+++ components/dashboard/Pelatihan/TableDataPesertaPelatihan.tsx
@@ -1124,6 +1124,33 @@ const TableDataPesertaPelatihan = () => {
               type={"short"}
             />
 
+            <div className="flex items-center justify-end space-x-2 py-4 text-[#e1e7ef]">
+              <div className="text-muted-foreground flex-1 text-sm">
+                {table.getFilteredSelectedRowModel().rows.length} of{" "}
+                {table.getFilteredRowModel().rows.length} row(s) selected.
+              </div>
+              <div className="space-x-2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="font-inter"
+                  onClick={() => table.previousPage()}
+                  disabled={!table.getCanPreviousPage()}
+                >
+                  Previous
+                </Button>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="font-inter"
+                  onClick={() => table.nextPage()}
+                  disabled={!table.getCanNextPage()}
+                >
+                  Next
+                </Button>
+              </div>
+            </div>
+
             {data != null && dataPelatihan != null ? (
               emptyFileSertifikatCount > 0 &&
               dataPelatihan.StatusPenerbitan == "" && (
