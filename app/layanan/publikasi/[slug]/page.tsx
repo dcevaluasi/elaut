5bf325737d806b56897bdfc4f57a4203940c59cf
gustonecrush <codeboxezz@gmail.com>
2025-05-19 19:53:21 +0900

    push

--- app/layanan/publikasi/[slug]/page.tsx
+++ app/layanan/publikasi/[slug]/page.tsx
@@ -223,7 +223,7 @@ function PDFViewerPublication({ fileUrl }: { fileUrl: string }) {
 const PdfViewer = ({ url }: { url: string }) => {
     const defaultLayoutPluginInstance = defaultLayoutPlugin();
     return (
-        <div className="h-screen w-screen">
+        <div className="h-screen w-full">
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js">
                 <Viewer
                     fileUrl={url}
