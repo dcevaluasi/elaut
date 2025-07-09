eee3bd7cb96b9db0e20a2b8a6039d30bf5d38980
gustonecrush <codeboxezz@gmail.com>
2025-07-10 02:41:33 +0700

    push

--- constants/urls.ts
+++ constants/urls.ts
@@ -12,6 +12,6 @@ export const wilayahIDUrl = process.env.NEXT_PUBLIC_WILAYAH_ID_URL
 export const verifyPDFBSrEUrl = 'https://tte.komdigi.go.id/verifyPDF'
 
 export const urlFileSilabus =
-  process.env.NEXT_PUBLIC_BASE_URL + '/public/static/silabusPelatihan'
+  process.env.NEXT_PUBLIC_BASE_URL + '/public/static/suratPemberitahuan'
 export const urlFileSuratPemberitahuan =
   process.env.NEXT_PUBLIC_BASE_URL + '/public/static/suratPemberitahuan'
