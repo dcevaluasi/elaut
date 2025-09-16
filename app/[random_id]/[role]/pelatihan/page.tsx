51f428a9cc27adc8e17d6b58c44a2882a6e3b650
gustonecrush <codeboxezz@gmail.com>
2025-09-16 21:40:51 +0700

    push

--- app/[random_id]/[role]/pelatihan/page.tsx
+++ app/[random_id]/[role]/pelatihan/page.tsx
@@ -6,15 +6,13 @@ import { TbSchool } from "react-icons/tb";
 
 export default function Page() {
   return (
-    <>
-      <LayoutAdminElaut>
-        <section className="flex-1 flex flex-col">
-          <HeaderPageLayoutAdminElaut title="Penyelenggaraan Pelatihan" description=" Monitoring dan kelola penyelenggaran pelatihan kelautan dan perikanan mulai buka kelas hingga penerbitan STTPL!" icon={<TbSchool className="text-3xl" />} />
-          <main className="w-full h-full">
-            <Pelatihan />
-          </main>
-        </section>
-      </LayoutAdminElaut>
-    </>
+    <LayoutAdminElaut>
+      <section className="flex-1 flex flex-col">
+        <HeaderPageLayoutAdminElaut title="Penyelenggaraan Pelatihan" description=" Monitoring dan kelola penyelenggaran pelatihan kelautan dan perikanan mulai buka kelas hingga penerbitan STTPL!" icon={<TbSchool className="text-3xl" />} />
+        <main className="w-full h-full">
+          <Pelatihan />
+        </main>
+      </section>
+    </LayoutAdminElaut>
   );
 }
